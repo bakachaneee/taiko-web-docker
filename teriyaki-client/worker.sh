735e12c4e352ebc61fb21d6797f719ad107b036c
bakachaneee <o34jfpoko@outlook.jp>
2023-02-26 21:42:59 +0900

    install all songs from teriyaki server

---
+++ teriyaki-client/worker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wait-for-it -h "$DB_HOST" -p "$DB_PORT"
+size=$(curl https://teriyaki-server.lolichanx.repl.co | jq -r "length")
+for id in $(seq "$size"); do
+    curl -o "$id.json" "https://teriyaki-server.lolichanx.repl.co/db/$id.json"
+    mongoimport --uri "mongodb://$DB_HOST:$DB_PORT" --file "$id.json" --db taiko --collection songs
+done
