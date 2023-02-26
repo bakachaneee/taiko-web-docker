d20c11601652f2601408f806d9765226ee8d5b7a
bakachaneee <o34jfpoko@outlook.jp>
2023-02-26 22:10:04 +0900

    dont save file if fail to fetch from teriyaki

--- teriyaki-client/worker.sh
+++ teriyaki-client/worker.sh
@@ -3,6 +3,6 @@
 wait-for-it -h "$DB_HOST" -p "$DB_PORT"
 size=$(curl https://teriyaki-server.lolichanx.repl.co | jq -r "length")
 for id in $(seq "$size"); do
-    curl -o "$id.json" "https://teriyaki-server.lolichanx.repl.co/db/$id.json"
+    curl -f -o "$id.json" "https://teriyaki-server.lolichanx.repl.co/db/$id.json"
     mongoimport --uri "mongodb://$DB_HOST:$DB_PORT" --file "$id.json" --db taiko --collection songs
 done
